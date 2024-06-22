d4ecc6efed19ccfeb2dbbc4a526edcee6fc3718a
Sergio Esteban Pola Contreras <sergio.pola.contreras@gmail.com>
2024-06-21 20:15:08 -0400

    Incluir en swagger

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { DiskHealthIndicator, HealthCheck, HealthCheckService, HttpHealthIndicator, TypeOrmHealthIndicator } from '@nestjs/terminus';
 
+@ApiTags("health")
 @Controller('health')
 export class HealthController {
     constructor(
