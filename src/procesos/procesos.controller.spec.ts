7156a7458e4de96ba784919ddcd0e95ee05df784
Sergio Esteban Pola Contreras <sergio.pola.contreras@gmail.com>
2024-06-21 21:53:00 -0400

    Hacemos funcionar los test

--- src/procesos/procesos.controller.spec.ts
+++ src/procesos/procesos.controller.spec.ts
@@ -1,20 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProcesosController } from './procesos.controller';
 import { ProcesosService } from './procesos.service';
+import { Proceso } from './entities/proceso.entity';
+import { CreateProcesoDto } from './dto/create-proceso.dto';
 
 describe('ProcesosController', () => {
   let controller: ProcesosController;
+  let service: ProcesosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProcesosController],
-      providers: [ProcesosService],
+      providers: [{
+        provide: ProcesosService,
+        useValue: {
+          create: jest.fn().mockResolvedValue(new Proceso())
+        }
+      }],
     }).compile();
 
     controller = module.get<ProcesosController>(ProcesosController);
+    service = module.get<ProcesosService>(ProcesosService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  it('should be created', async () => {
+
+    let dto = {
+    } as CreateProcesoDto;
+
+    let resp = controller.create(dto);
+    await expect(resp).resolves.toBeInstanceOf(Proceso);
   });
 });
