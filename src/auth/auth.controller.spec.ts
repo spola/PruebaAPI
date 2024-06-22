2806bf75f1c5a452ce3652647a3cc539662a9242
Sergio Esteban Pola Contreras <sergio.pola.contreras@gmail.com>
2024-06-21 20:15:23 -0400

    Hacer funcionar los tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let userService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn().mockResolvedValue({
+              userId: 1,
+              username: 'john',
+              password: 'changeme',
+            })
+          }
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn().mockResolvedValue("el_token")
+          }
+        },
+        AuthService,
+      ]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  it('should return correct token', async () => {
+    let dto = {
+      username: 'john',
+      password: 'changeme',
+    };
+
+    let resp = controller.signIn(dto);
+    await expect(resp).resolves.toHaveProperty('access_token', "el_token");
+  });
+
+  it('should throw UnauthorizedException', async () => {
+    let dto = {
+      username: 'john',
+      password: 'changeme',
+    };
+
+    jest.spyOn(userService, "findOne").mockResolvedValueOnce({
+      username: 'john',
+      password: 'otra_password'
+    });
+
+    let resp = controller.signIn(dto);
+    await expect(resp).rejects.toThrow(UnauthorizedException);
   });
 });
--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { HealthCheckService, TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          ignoreEnvVars: false,
+          ignoreEnvFile: false,
+          //load: [MockEnvConfig],
+        }),
+        TerminusModule,
+        HttpModule
+      ],
       controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: {
+            check: jest.fn().mockResolvedValue({
+              status: "ok"
+            }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
@@ -14,5 +36,11 @@ describe('HealthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should return happy result', async () => {
+    let checked = controller.check();
+    await expect(checked).resolves.toHaveProperty('status');
+
+    
   });
 });
